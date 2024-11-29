c1d05fd5f69a62b0d4cc459701038c59a56eec19
Tarun_Silam <tarun79767@gmail.com>
2024-11-29 21:22:10 +0530

    changes

--- School_Management/routes/schoolRouter.js
+++ School_Management/routes/schoolRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 dotenv.config();
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST,           
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
